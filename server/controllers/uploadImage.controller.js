fd1ba7720e68f2e618e73fdac67b6164e36e1f60
Kaushal Patel <kaushalpatel531.kp@gmail.com>
2024-11-18 11:56:05 +0530

    Fix: Fix an issue for import lib and connect db syntax.

--- server/controllers/uploadImage.controller.js
+++ server/controllers/uploadImage.controller.js
@@ -1,4 +1,4 @@
-import uploadImageClodinary from "../utils/uploadImageClodinary.js"
+import uploadImageClodinary from '../utils/uploadImageCloudinary.js'
 
 const uploadImageController = async(request,response)=>{
     try {
