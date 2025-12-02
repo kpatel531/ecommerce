f98b061d653a4512cd99cd777c06c4c972919f4d
Kaushal Patel <kaushalpatel531.kp@gmail.com>
2025-12-02 21:19:10 +0530

    Development: Update static url to make dynamic config value for setting plateform.

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -146,7 +146,6 @@ const getOrderProductItems = async({
     return productList
 }
 
-//http://localhost:8080/api/order/webhook
 export async function webhookStripe(request,response){
     const event = request.body;
     const endPointSecret = process.env.STRIPE_ENPOINT_WEBHOOK_SECRET_KEY
