5da183e71fdbe3485754836068d533e4ef951759
Kaushal Patel <kaushalpatel531.kp@gmail.com>
2025-12-05 08:15:55 +0530

    Development: Fix layout phase-1.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import banner from '../assets/banner.jpg'
+import banner from '../assets/mainbanner.png'
 import bannerMobile from '../assets/banner-mobile.jpg'
 import { useSelector } from 'react-redux'
 import { valideURLConvert } from '../utils/valideURLConvert'
