f9edd00c510482d69dd930083dc440080b6020e2
Kaushal Patel <kaushalpatel531.kp@gmail.com>
2025-12-17 08:16:12 +0530

    Development: Fix page route option with rewrite rules.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,15 @@ import { store } from './store/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider
+        router={router}
+        future={{
+          v7_partialHydration: true,
+          v7_startTransition: true,
+          v7_fetcherPersist: true,
+          v7_relativeSplatPath: true
+        }}
+      />
     </Provider>
   </StrictMode>,
 )
